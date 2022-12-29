318a4f9edfcaa8a68093b36565998eb4610e5491
blank2113 <hollywoodthere@gmail.com>
2022-12-29 18:35:36 +0500

    need to add style for scroll-bar

--- src/components/bestProducts/BestProducts.jsx
+++ src/components/bestProducts/BestProducts.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./bestProducts.css";
 import "swiper/css";
@@ -7,57 +7,54 @@ import "swiper/css/navigation";
 import "swiper/css/scrollbar";
 import SwiperCore, { Scrollbar } from "swiper/core";
 import { Pagination, Navigation } from "swiper";
-import { useGetBestProductsQuery} from '../../store/bestProductApi'
-
-
-
+import { useGetBestProductsQuery } from "../../store/bestProductApi";
 
 function BestProducts() {
-    const {data =[]} = useGetBestProductsQuery();
+  const { data = [] } = useGetBestProductsQuery();
   return (
     <section className="best-products">
-    <div className="container">
-      <h4 className="title">Самые востребованные товары</h4>
-      <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        cssMode={true}
-        scrollbar={{ draggable: true, dragSize: 240 }}
-        navigation={true}
-        modules={[Pagination, Navigation, Scrollbar]}
-        breakpoints={ {
-  // when window width is >= 320px
-  320: {
-    slidesPerView: 1,
-    spaceBetween: 30
-  },
-  550: {
-    slidesPerView: 2,
-    spaceBetween: 30
-  },
-  // when window width is >= 480px
-  1024: {
-    slidesPerView: 3,
-    spaceBetween: 30
-  },
-  // when window width is >= 640px
-  1200: {
-    slidesPerView: 4,
-    spaceBetween: 30
-  }
-}}
-        className="swiper"
-      >
-        {data.map((item) => (
-          <SwiperSlide className="swiper-card" key={item.id}>
-            <img src={item.picture} alt="bestProducts-img"/>
-            <p className="swiper-card__curr">{item.price} сум</p>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  </section>
-  )
+      <div className="container">
+        <h4 className="title">Самые востребованные товары</h4>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          cssMode={true}
+          scrollbar={{ draggable: true, dragSize: 240 }}
+          navigation={true}
+          modules={[Pagination, Navigation, Scrollbar]}
+          breakpoints={{
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            },
+            550: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            // when window width is >= 480px
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            // when window width is >= 640px
+            1200: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+          className="swiper"
+        >
+          {data.map((item) => (
+            <SwiperSlide className="swiper-card" key={item.id}>
+              <img src={item.picture} alt="bestProducts-img" />
+              <p className="swiper-card__curr">{item.price} сум</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 }
 
-export default BestProducts
+export default BestProducts;
--- src/components/nav/navItem/NavItem.jsx
+++ src/components/nav/navItem/NavItem.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { motion } from "framer-motion";
 import ActiveLine from "../activeLine/ActiveLine";
-import "./navItem.css"
+import "./navItem.css";
 import { Link } from "react-scroll";
-
 
 function NavItem(props) {
   const { item, isSelected, handleClick = Function.prototype } = props;
-console.log(item);
+  console.log(item);
   return (
     <motion.div
       onHoverStart={handleClick}
-      initial={{ color: "#000" }}
-      animate={{ color: isSelected  ? "rgba(255,255,255)" : "#000" }}
+      // initial={{ color: "rgba(255,255,255)" }}
+      // animate={{
+      //   color: isSelected ? "rgba(255,255,255)" : "rgba(255,255,255)",
+      // }}
       className="nav-content__list list"
       style={{ position: "relative" }}
     >
-      {isSelected && <ActiveLine /> }
-      {<Link  
-    to={item.href}
-    spy={true}
-    smooth={true}
-    offset={-70}
-    duration= {500} >{item.name}</Link>}
-      
+      {isSelected && <ActiveLine />}
+      {
+        <Link
+          to={item.href}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          {item.name}
+        </Link>
+      }
     </motion.div>
   );
 }
