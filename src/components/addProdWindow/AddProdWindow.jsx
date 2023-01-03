8ad49e829e0fb41ec0d76b91617a463b661b5816
blank2113 <hollywoodthere@gmail.com>
2023-01-03 20:29:28 +0500

    still work on connection

--- src/components/addProdWindow/AddProdWindow.jsx
+++ src/components/addProdWindow/AddProdWindow.jsx
@@ -1,59 +1,61 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./addProdWindow.css";
 import { useDispatch } from "react-redux";
 import { motion } from "framer-motion";
 import { getStatusValue } from "../../store/slices/getStatus";
 import axios from "axios";
-import { useAddNewProductMutation} from '../../store/middlewares/productsApi'
+import { useAddNewProductMutation } from "../../store/middlewares/productsApi";
 
 function AddProdWindow() {
-  const [addNewProd, response] = useAddNewProductMutation();
-  const [data, setData] = useState({
-    name:"",
-    price:""
-  })
+  // const [addNewProd, response] = useAddNewProductMutation();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
   // const [addNewAnimals] = useAddNewAnimalsMutation();
-  
-  const dispatch = useDispatch();
-  
-  const onSubmit = async (e) => {
-    e.preventDefault()
-    const { name, price } = e.target.elements
-    let formData = {
-      name: name.value,
-      price: price.value,
-    }
-    await addNewProd(formData)
-      .unwrap()
-      .then(() => {})
-      .then((error) => {
-        console.log(error)
-      })
-  }
 
-// const handleSubmit = async ()=>{
-//   if(data.name){
-//     await addNewAnimals({name: data}).unwrap();
-//     setData('');
-//   }
-// }
-// let formData = new FormData();
-  
-// console.log(formData);
-//   const  handleSubmit = (event)=> {
-//     event.preventDefault();
-//     formData.append('name',data.name)
-//     formData.append("price", Number(data.price))
-   
-//     // JSON.stringify(formData)
-//     axios({
-//       method: "post",
-//       url: "http://164.92.147.133:8000/products",
-//       data: formData,
-//       headers: { "Content-Type": "multipart/form-data" },
-//     }).then(res => console.log(res)).catch(e => console.log(e))
-//   }
-  
+  const dispatch = useDispatch();
+
+  // const onSubmit = async (e) => {
+  //   e.preventDefault()
+  //   const { name, price } = e.target.elements
+  //   let formData = {
+  //     name: name.value,
+  //     price: price.value,
+  //   }
+  //   await addNewProd(formData)
+  //     .unwrap()
+  //     .then(() => {})
+  //     .then((error) => {
+  //       console.log(error)
+  //     })
+  // }
+
+  // const handleSubmit = async ()=>{
+  //   if(data.name){
+  //     await addNewAnimals({name: data}).unwrap();
+  //     setData('');
+  //   }
+  // }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("name", name);
+    formData.append("price", Number(price));
+    formData.append("status_id", '');
+    formData.append("brand_id", '');
+    formData.append("type_id", '');
+    formData.append("category_id", '');
+    formData.append("image", '');
+
+    console.log(formData);
+    // JSON.stringify(formData)
+    axios({
+      method: "post",
+      url: "http://164.92.147.133:8000/product",
+      data: formData,
+      // headers: { "Content-Type": "multipart/form-data" },
+    }).then(res => console.log(res)).catch(e => console.log(e))
+  };
 
   return (
     <motion.div
@@ -66,10 +68,19 @@ function AddProdWindow() {
         whileInView={{ opacity: 1, scale: 1 }}
         className="addProdWindow-inner"
       >
-        <form action="" onSubmit={onSubmit}>
-          <input type="text"  name="name" id="name" />
-          <input type="number" name="price"  />
-          <button type="submit" >send</button>
+        <form>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="number"
+            name="price"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <button onClick={handleSubmit}>send</button>
         </form>
         <button onClick={() => dispatch(getStatusValue(false))}>close</button>
       </motion.div>
